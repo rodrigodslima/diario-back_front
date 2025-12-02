b6715e2835d5e59ac401eb77b5441bd2929ce9f2
rodrigodslima <rodrigodanidsl@gmail.com>
2025-12-01 23:42:40 -0300

    Diário pessoal back e front

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Cadastro from "./components/Cadastro/Cadastro";
+
+function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: "Arial, sans-serif" }}>
+      <h1>Diário Pessoal</h1>
+      <Cadastro />
+    </div>
+  );
+}
+
+export default App;
+
